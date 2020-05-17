2ef0d69aa08f0437b76f180767fd30405076210c
nguyen.van.dungc <nguyen.van.dungc@sun-asterisk.com>
2020-05-17 23:21:02 +0700

    API Position

--- apps/position/urls.py
+++ apps/position/urls.py
@@ -1,4 +1,13 @@
 from django.urls import path
-from . import views as auth_views
+from rest_framework.urlpatterns import format_suffix_patterns
+from apps.position import views
 
 app_name = 'position'
+
+urlpatterns = [
+    path('positions/', views.PositionList.as_view()),
+    path('positions/<int:pk>/', views.PositionDetail.as_view()),
+    path('positions/import-csv/', views.ImportCsv.as_view()),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
