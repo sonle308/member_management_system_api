31044be9da6a19f061bf85823f7786a0b37fb2d4
nguyen.van.dungc <nguyen.van.dungc@sun-asterisk.com>
2020-05-17 22:53:54 +0700

    API User Import Export CSV

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,16 +1,22 @@
 from django.conf.urls import url, include
+from django.http.multipartparser import MultiPartParser
 from rest_framework import viewsets, mixins
 from apps.user.serializers import UserSerializer
 from django.contrib.auth import get_user_model
 from django.http import Http404
 from rest_framework.views import APIView
+from rest_framework_csv.renderers import CSVRenderer
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.settings import api_settings
+from rest_framework_csv import renderers as r
 
 User = get_user_model()
 
 
 class UserList(APIView):
+    renderer_classes = (r.CSVRenderer, ) + tuple(api_settings.DEFAULT_RENDERER_CLASSES)
+
     @classmethod
     def get_extra_actions(cls):
         return []
@@ -27,6 +33,13 @@ class UserList(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+    def get_renderer_context(self):
+        context = super().get_renderer_context()
+        context['header'] = (
+            self.request.GET['fields'].split(',')
+            if 'fields' in self.request.GET else None)
+        return context
 
 class UserDetail(APIView):
     def get_object(self, pk):
@@ -52,3 +65,27 @@ class UserDetail(APIView):
         user = self.get_object(pk)
         user.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class FormParser(object):
+    pass
+
+
+class ImportCsv(APIView):
+    serializer_class = UserSerializer
+    parser_classes = [ MultiPartParser,FormParser ]
+
+    def post(self,request):
+        try:
+            serializer = UserSerializer(data=request.data)
+            print(serializer.initial_data)
+
+            if serializer.is_valid():
+                print(serializer.data)
+                return Response("Done")
+            else:
+                print(serializer.errors)
+                return Response("Not Done")
+
+        except Exception as e:
+            return Response(str(e))
